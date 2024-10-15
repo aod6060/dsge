bbde7e2a82400cedeb18ef6c81141e040db7fef5
Frederick R. Cook <frcook.6060@gmail.com>
2024-10-15 17:47:04 -0500

    Add some stuff to give dev for clangd usable.

--- src/engine/sys.h
+++ src/engine/sys.h
@@ -2,6 +2,19 @@
 #define SYS_H
 
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <functional>
+#include <algorithm>
+#include <random>
+#include <chrono>
+#include <filesystem>
+
+#include <SDL.h>
 
 #include "version.h"
 
