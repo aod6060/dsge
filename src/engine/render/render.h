536b22fd26ec0bbfa89aedfc042db779ec469d8a
Frederick R. Cook <frcook.6060@gmail.com>
2024-10-18 21:33:31 -0500

    Added some aditional content

--- src/engine/render/render.h
+++ src/engine/render/render.h
@@ -12,6 +12,7 @@ namespace render {
     void startFrame();
     void endFrame();
 
+    // Note this shouldn't be part of the public api.
     void setProjection(const glm::mat4& m);
     void setView(const glm::mat4& m);
     void setModel(const glm::mat4& m);
@@ -20,8 +21,8 @@ namespace render {
 
     /*
         The render will draw at 
-        640x480 if w/h * 3 == 4 or standard screen
-        640x360 if w/h * 9 == 11   wide screen
+        640x480 if w/h * 3 == 4 or standard screen (4:3)
+        640x360 if w/h * 9 == 11   wide screen     (16:9)
     */
     uint32_t getWidth();
     uint32_t getHeight();
