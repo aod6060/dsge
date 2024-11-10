7754495c8cd1c2a36d28b34d012bd550bcff774d
Frederick R. Cook <frcook.6060@gmail.com>
2024-11-09 23:37:56 -0600

    Turing in for tonight. Problems with cpBoxShapeNew2

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "chipmunk/chipmunk_types.h"
 #include "engine/app/app.h"
 #include "engine/input/input.h"
 #include "engine/render/font/font.h"
@@ -8,6 +9,7 @@
 #include "glm/ext/matrix_transform.hpp"
 #include "glm/ext/quaternion_geometric.hpp"
 #include "glm/ext/vector_common.hpp"
+#include "glm/ext/vector_float2.hpp"
 #include "glm/gtc/constants.hpp"
 #include "glm/trigonometric.hpp"
 #include <cmath>
@@ -19,11 +21,15 @@
 #include "thirdparty/imgui/imgui.h"
 #include "thirdparty/imgui/imgui_impl_sdl2.h"
 #include "thirdparty/imgui/imgui_impl_opengl3.h"
+#include <chipmunk/chipmunk.h>
 
 struct TestApplication : public app::IApplication {
         int i = 0;
 
         render::glw::Texture2D icon_32;
+        render::glw::Texture2D box_tex;
+        render::glw::Texture2D brick_tex;
+
         render::glw::Texture2DArray icon_array;
 
         // Custom TexCoords for texture array example
@@ -43,9 +49,27 @@ struct TestApplication : public app::IApplication {
         int32_t width;
         int32_t height;
 
-        
-        virtual void init() {
+        cpVect gravity;
+        cpSpace* space = nullptr;
 
+        // Platform
+        //cpBB* platform = nullptr;
+        cpBB box;
+
+        cpShape* platformShape = nullptr;
+        cpBody* platformBody = nullptr;
+        // Player
+        // Box
+        cpShape* boxShape = nullptr;
+        cpBody* boxBody = nullptr;
+
+
+        float time = 0.0f;
+        float maxTime = 1.0f / 60.0f;
+
+        bool isMouseEnabled = false;
+
+        virtual void init() {
             IMGUI_CHECKVERSION();
 
             ImGui::CreateContext();
@@ -60,6 +84,8 @@ struct TestApplication : public app::IApplication {
 
 
             render::glw::Texture2D::createTexture2DFromFile(&icon_32, "data/icon/icon_32.png");
+            render::glw::Texture2D::createTexture2DFromFile(&this->box_tex, "data/icon/Box.png");
+            render::glw::Texture2D::createTexture2DFromFile(&this->brick_tex, "data/icon/Brick.png");
 
             render::glw::Texture2DArray::createTexture2DArrayFromFiles(&icon_array, {
                 "data/icon/icon_32.png",
@@ -77,6 +103,42 @@ struct TestApplication : public app::IApplication {
 
 
             render::font::loadFont("regular", "data/font/londrina_sketch_regular.ttf", 64);
+
+            this->gravity = cpv(0.0f, 100.0f);
+
+            this->space = cpSpaceNew();
+            cpSpaceSetGravity(space, this->gravity);
+
+            // Platform (64.0f, 32.0f * 12) [32.0f * 16.0f, 32.0f]
+            platformBody = cpBodyNewStatic();
+            cpBodySetPosition(platformBody, cpv(64.0f, 32.0f * 12.0f));
+            cpBodySetAngle(platformBody, 0.0f);
+        
+            box.l = 0.0f;
+            box.r = 32.0f * 16.0f;
+            box.t = 0.0f;
+            box.b = 32.0f;
+
+            platformShape = cpBoxShapeNew2(this->platformBody, box, 1.0f);
+            //platformShape = cpBoxShapeNew(this->platformBody, 32.0f * 16.0f, 32.0f, 1.0f);
+            cpSpaceAddShape(this->space, this->platformShape);
+            //cpShapeSetFriction(this->platformShape, 1.0f);
+            
+            // Player
+            
+            // Box
+            cpFloat moment = cpMomentForBox(1.0f, 32.0f, 32.0f);
+
+            //this->boxBody = cpBodyNew(1.0f, moment);
+
+            this->boxBody = cpSpaceAddBody(space, cpBodyNew(1.0f, moment));
+            cpBodySetPosition(this->boxBody, cpv(128.0f, 32.0f));
+
+            //this->boxShape = cpSpaceAddShape(space, cpBoxShapeNew(this->boxBody, 32.0f, 32.0f, 1.0f));
+            this->boxShape = cpSpaceAddShape(space, cpBoxShapeNew2(boxBody, cpBBNew(0.0f, 32.0f, 32.0f, 0.0f), 1.0f));
+
+
+            //cpShapeSetFriction(this->boxShape, 0.7f);
         }
 
         virtual void handleEvent(SDL_Event* e) {
@@ -110,10 +172,30 @@ struct TestApplication : public app::IApplication {
             // Add it to the postion
             this->postion += velocity * speed * delta;
 
-
-            if(input::isKeyPressedOnce(input::Keyboard::KEYS_TAB)) {
-                isArrayTest = !isArrayTest;
+            if(this->time >= this->maxTime) {
+                //cpSpaceStep(space, 1.0f / 60.0f);
+                this->time = 0.0f;
+            } else {
+                this->time += delta;
             }
+
+            if(isMouseEnabled) {
+                if(input::isMouseButtonPressedOnce(input::MouseButtons::MBS_LEFT) && !input::isKeyPressed(input::Keyboard::KEYS_LCTRL)) {
+                    // Scale Mouse Position
+                    float scaledWidth = (float)app::get_width() / (float)render::getWidth();
+                    float scaledHeight = (float)app::get_height() / (float)render::getHeight();
+
+                    glm::vec2 mp = input::getPosition();
+
+                    glm::vec2 mc = glm::vec2(
+                        mp.x / scaledWidth,
+                        mp.y / scaledHeight
+                    );
+
+                    cpBodySetPosition(this->boxBody, cpv(mc.x, mc.y));
+                }
+            }
+            cpSpaceStep(space, 1.0f / 60.0f);
         }
 
         void renderMenu() {
@@ -125,6 +207,8 @@ struct TestApplication : public app::IApplication {
 
             ImGui::Text("Physics System Controls");
 
+            ImGui::Checkbox("Is Box Mouse Enabled", &this->isMouseEnabled);
+
             ImGui::End();
 
             ImGui::EndFrame();
@@ -132,6 +216,23 @@ struct TestApplication : public app::IApplication {
             ImGui::Render();
 
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        }
+
+        void drawSprite(render::glw::Texture2D* tex, const glm::vec2& position, const glm::vec2& size, float zrot) {
+            render::startShader(render::ShaderType::ST_MAIN);
+
+            //render::setView(glm::mat4(1.0f));
+            render::setModel(
+                glm::translate(glm::mat4(1.0f), glm::vec3(position, 0.0f)) *
+                glm::rotate(glm::mat4(1.0f), glm::radians(zrot), glm::vec3(0.0f, 0.0f, 1.0f)) *
+                glm::scale(glm::mat4(1.0f), glm::vec3(size, 0.0f))
+            );
+
+            tex->bind(GL_TEXTURE0);
+            render::draw();
+            tex->unbind(GL_TEXTURE0);
+
+            render::endShader();
         }
 
         virtual void render() {
@@ -142,38 +243,16 @@ struct TestApplication : public app::IApplication {
             render::setView(glm::mat4(1.0f));
             render::updateCameraBuffer();
 
+            cpVect p = cpBodyGetPosition(this->boxBody);
 
-            if(isArrayTest) {
-                render::startShader(render::ShaderType::ST_TEXTURE2D_ARRAY);
+            this->drawSprite(&this->box_tex, glm::vec2(p.x, p.y), glm::vec2(32.0f), 0.0f);
 
-                //render::setView(glm::mat4(1.0f));
-                render::setModel(
-                    glm::translate(glm::mat4(1.0f), glm::vec3(this->postion, 0.0f)) *
-                    glm::scale(glm::mat4(1.0f), glm::vec3(32.0f, 32.0f, 0.0f))
-                );
+            p = cpBodyGetPosition(this->platformBody);
 
-                icon_array.bind(GL_TEXTURE0);
-                render::draw(icon_array_texCoords);
-                icon_array.unbind(GL_TEXTURE0);
+            this->drawSprite(&this->brick_tex, glm::vec2(p.x, p.y), glm::vec2(32.0f * 16.0f, 32.0f), 0.0f);
 
-                render::endShader();
+            this->drawSprite(&this->icon_32, this->postion, glm::vec2(32.0f, 32.0f), 0.0f);
 
-            } else {
-                render::startShader(render::ShaderType::ST_MAIN);
-
-                //render::setView(glm::mat4(1.0f));
-                render::setModel(
-                    glm::translate(glm::mat4(1.0f), glm::vec3(this->postion, 0.0f)) *
-                    glm::scale(glm::mat4(1.0f), glm::vec3(32.0f, 32.0f, 0.0f))
-                );
-
-                icon_32.bind(GL_TEXTURE0);
-                render::draw();
-                icon_32.unbind(GL_TEXTURE0);
-
-                render::endShader();
-            }
-            
             render::endFrame();
 
             
@@ -209,6 +288,20 @@ struct TestApplication : public app::IApplication {
         }
 
         virtual void release() {
+            cpSpaceRemoveShape(this->space, this->boxShape);
+            cpSpaceRemoveBody(this->space, this->boxBody);
+            cpSpaceRemoveShape(this->space, this->platformShape);
+
+            cpShapeFree(this->boxShape);
+            cpBodyFree(this->boxBody);
+
+            cpShapeFree(this->platformShape);
+            cpBodyFree(this->platformBody);
+            // Freeing Chipmunk Stuff
+            cpSpaceFree(this->space);
+            // Forget to release textures :(
+            box_tex.release();
+            brick_tex.release();
             icon_array_texCoords.release();
             icon_array.release();
             icon_32.release();
